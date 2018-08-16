2bc914d7ea7edd9b1844df3049457c2f99859718
bio-tarik <bio.tarik@gmail.com>
2018-08-16 09:46:22 -0300

    Innitial commit

---
+++ src/components/firstComponent/Button.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 }
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1
+    }))
+  };
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.props.label}: {this.state.counter}
+      </button>
+    );
+  }
+}
+
+export default Button;
