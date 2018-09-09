c2e95e07ec642e4a81f03fb1cc4ed39d6dde7c8c
Tarik Bet Ayoub <bio.tarik@gmail.com>
2018-09-08 23:57:00 -0300

    Adding ways to win reset and lose the game

--- src/play-nine/src/components/game/game.js
+++ src/play-nine/src/components/game/game.js
@@ -3,28 +3,115 @@ import Stars from '../stars/stars';
 import Button from '../button/button';
 import Answer from '../answer/answer';
 import Numbers from '../numbers/numbers';
+import DoneFrame from '../doneframe/doneframe';
+
+const possibleCombinationSum = (arr, n) => {
+  if (arr.indexOf(n) >= 0) { return true; }
+  if (arr[0] > n) { return false; }
+  if (arr[arr.length - 1] > n) {
+    arr.pop();
+    return possibleCombinationSum(arr, n);
+  }
+  const listSize = arr.length, combinationsCount = (1 << listSize)
+  for (let i = 1; i < combinationsCount; i++) {
+    let combinationSum = 0;
+    for (let j = 0; j < listSize; j++) {
+      if (i & (1 << j)) { combinationSum += arr[j]; }
+    }
+    if (n === combinationSum) { return true; }
+  }
+  return false;
+};
 
 class Game extends Component {
-  state = {
+  static randomNumber = () => 1 + Math.floor(Math.random() * 9);
+  static initialState = () => ({
     selectedNumbers: [],
-    randomNumberOfStars: 1 + Math.floor(Math.random() * 9)
-  };
+    randomNumberOfStars: Game.randomNumber(),
+    answerIsCorrect: null,
+    usedNumbers: [],
+    redraws: 5,
+    doneStatus: null,
+  });
+
+  state = Game.initialState();
+
+  resetGame = () => this.setState(Game.initialState());
 
   selectNumber = (clickedNumber) => {
     if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) { return; }
     this.setState(prevState => ({
+      answerIsCorrect: null,
       selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
     }));
   };
 
   unselectNumber = (clickedNumber) => {
     this.setState(prevState => ({
+      answerIsCorrect: null,
       selectedNumbers: prevState.selectedNumbers.filter(n => n !== clickedNumber)
     }))
   };
 
+  checkAnswer = () => {
+    this.setState(prevState => ({
+      answerIsCorrect: prevState.randomNumberOfStars ===
+        prevState.selectedNumbers.reduce((acc, n) => acc + n, 0)
+    }));
+  };
+
+  acceptAnswer = () => {
+    this.setState(prevState => ({
+      usedNumbers: prevState.usedNumbers.concat(prevState.selectedNumbers),
+      selectedNumbers: [],
+      answerIsCorrect: null,
+      randomNumberOfStars: Game.randomNumber(),
+    }), this.updateDoneStatus);
+  };
+
+  redraw = () => {
+    if (this.state.redraws === 0) {
+      return;
+    }
+
+    this.setState(prevState => ({
+      randomNumberOfStars: Game.randomNumber(),
+      answerIsCorrect: null,
+      selectedNumbers: [],
+      redraws: prevState.redraws - 1
+    }), this.updateDoneStatus);
+  };
+
+  possibleSolutions = ({ randomNumberOfStars, usedNumbers }) => {
+    const numbersList = Array.from(Array(9).keys(), n => n + 1);
+    const possibleNumbers = numbersList.filter(n =>
+      usedNumbers.indexOf(n) === -1
+    );
+
+    return possibleCombinationSum(possibleNumbers, randomNumberOfStars);
+  };
+
+  updateDoneStatus = () => {
+    this.setState(prevState => {
+      if (prevState.usedNumbers.length === 9) {
+        return { doneStatus: 'Done. Nice!' };
+      }
+
+      if (prevState.redraws === 0 && !this.possibleSolutions(prevState)) {
+        return { doneStatus: 'Game Over!' };
+      }
+    });
+  };
+
   render() {
-    const { selectedNumbers, randomNumberOfStars } = this.state;
+    const {
+      selectedNumbers,
+      randomNumberOfStars,
+      answerIsCorrect,
+      usedNumbers,
+      redraws,
+      doneStatus,
+    } = this.state;
 
     return (
       <div className="container">
@@ -32,13 +119,22 @@ class Game extends Component {
         <hr></hr>
         <div className="row">
           <Stars numberOfStars={randomNumberOfStars} />
-          <Button selectedNumbers={selectedNumbers}/>
+          <Button selectedNumbers={selectedNumbers}
+            checkAnswer={this.checkAnswer}
+            acceptAnswer={this.acceptAnswer}
+            redraw={this.redraw}
+            redraws={redraws}
+            answerIsCorrect={answerIsCorrect} />
           <Answer selectedNumbers={selectedNumbers}
             unselectNumber={this.unselectNumber} />
         </div>
         <br />
-        <Numbers selectedNumbers={selectedNumbers}
-          selectNumber={this.selectNumber} />
+        {doneStatus ?
+          <DoneFrame resetGame={this.resetGame} doneStatus={doneStatus} /> :
+          <Numbers selectedNumbers={selectedNumbers}
+            selectNumber={this.selectNumber}
+            usedNumbers={usedNumbers} />
+        }
       </div>
     )
   }
--- src/play-nine/src/components/numbers/numbers.js
+++ src/play-nine/src/components/numbers/numbers.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Numbers = (props) => {
   const numberClassName = (number) => {
+    if (props.usedNumbers.indexOf(number) >= 0) {
+      return 'used';
+    };
     if (props.selectedNumbers.indexOf(number) >= 0) {
       return 'selected';
-    }
+    };
   };
 
   return (
